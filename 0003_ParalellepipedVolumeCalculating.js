ab238d18ae62ae7239ab281981db068ace19a046
Roman Khairov <roman.khairov@gmail.com>
2017-01-04 23:27:47 +0100

    Add file with function. Calculating volume of parallelepiped

---
+++ 0003_ParalellepipedVolumeCalculating.js
@@ -0,0 +1,6 @@
+//Function, that calculates volume of parallelepiped
+//It has few inputs
+var volumeParallelepiped = function (length, width, height) {
+    return length * width * height;
+    }
+    volumeParallelepiped(14,26,31); 
