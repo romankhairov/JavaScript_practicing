f69491b3cf94ec672f31bca17041abdf60955916
Roman Khairov <roman.khairov@gmail.com>
2017-01-11 22:59:50 +0100

    Add while loop for whole game

--- 0007_FootballerAtackAlgorithm.js
+++ 0007_FootballerAtackAlgorithm.js
@@ -2,7 +2,15 @@
 var playFootball = true;
 //defender- show us random position of defender in ranges- 
 //(0-0.33) = he locates in left side; (0.34-0.66)= locates if center; (0.67-1)= locates in the right side;  
-var defendeLocation = Math.random();
+var defenderLocation = Math.random();
+
+while (playFootball) {
+
+
+
+
+	playFootball= false;
+}
 
 
 
