15dc81aa121deec5933981d11c4173d9b01991d1
Roman Khairov <roman.khairov@gmail.com>
2017-01-11 22:53:12 +0100

    Add algorithm of football action during atack

---
+++ 0007_FootballerAtackAlgorithm.js
@@ -0,0 +1,9 @@
+//playFootball- variable, for play all time during game continues;
+var playFootball = true;
+//defender- show us random position of defender in ranges- 
+//(0-0.33) = he locates in left side; (0.34-0.66)= locates if center; (0.67-1)= locates in the right side;  
+var defendeLocation = Math.random();
+
+
+
+
