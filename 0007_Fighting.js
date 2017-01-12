cf323e48754fefceac8d840d7a08233735ef4977
Roman Khairov <roman.khairov@gmail.com>
2017-01-12 22:34:32 +0100

    Add comments

--- 0007_Fighting.js
+++ 0007_Fighting.js
@@ -1,8 +1,15 @@
+//This game is randomly (using Math.random) pick value of your hit
+//to the rival
+//Math.floor - is round a number downward to its nearest integer
+//Math.random- picks random number between 0 and 1
+// multyply to 2 is for definetly picking 0 or 1
 var fighting = true;
 var yourPunch = Math.floor(Math.random()*2);
 var damageToRival = Math.floor(Math.random()*2 + 1);
 var totalDamage= 0;
+
 while (fighting) {
+	//here we calculating of damage, summ it and compare to required damage for defeating the rival
     if (yourPunch) {
         console.log("You hit your rival!");
         totalDamage += damageToRival;
@@ -14,6 +21,8 @@ while (fighting) {
             } 
     } else {
         console.log("You were defeated by your rival... Try next time.");
+        //In While loop always should be variable that equals False 
+        //Otherwise it can bring infinite loop
         fighting = false;
     }
 }
