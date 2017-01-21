2005365764bd58f9c953ee0797805b93060b8394
Roman Khairov <roman.khairov@gmail.com>
2017-01-22 00:54:19 +0100

    Add cases for height and frame size

--- 0011_HowToChooseBicycle.js
+++ 0011_HowToChooseBicycle.js
@@ -1,8 +1,21 @@
+var height = prompt ("Hello! Here is a program, that will help you to choose a bike. Enter your height in centimeters:");
 
-var userChoice = prompt ("Hello! Here is a program, that will help you to choose a bike. Enter your height in centimeters:").Math.floor(n * 20) / 20;
-switch(userChoice) {
-    case '135', '140','145','150':
-        console.log('You need bicycle frame 14"');
+switch(height) {
+    case '135': case '136': case '137': case '138': case '139': case '140': case '141': case '142': case '143': case '144': case '145': case '146': case '147': case '148': case '149': case '150':
+        console.log('Bicycle frame 14" is for you');
+        break;
+    case '151': case '152': case '153': case '154': case '155': case '156': case '157': case '158': case '159': case '160': case '161': case '162': case '163': case '164': case '165':
+        console.log('Bicycle frame 16" is for you');
+        break;
+    case '166': case '167': case '168': case '169': case '170': case '171': case '172': case '173': case '174': case '175': case '176': case '177': case '178':
+        console.log('Bicycle frame 18" is for you');
+        break;
+    case '179': case '180': case '181': case '182': case '183': case '184': case '185':
+        console.log('Bicycle frame 20" is for you');
+        break;
+    case '186': case '187': case '188': case '189': case '190': case '192': case '193': case '194': case '195':
+        console.log('Bicycle frame 22" is for you');
         break;
     default:
-        console.log("Enter your real height");
+        console.log('Bicycle frame 24" is for you');
+      }
