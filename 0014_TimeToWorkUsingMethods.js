6eaee4ef4bd5b8533efe69fc9f6c967144372246
Roman Khairov <roman.khairov@gmail.com>
2017-02-04 22:03:22 +0100

    Use own constructor instead of  Object constructor

--- 0014_TimeToWorkUsingMethods.js
+++ 0014_TimeToWorkUsingMethods.js
@@ -1,8 +1,11 @@
-var roadTime = new Object ();
-roadTime.walk = 10;
-roadTime.bus52 = 5;
-roadTime.tram = 5;
-roadTime.bus62 = 10;
+function roadTime(walk, bus52, tram, bus62) {
+  this.walk = walk;
+  this.bus52 = bus52;
+  this.tram = tram;
+  this.bus62 = bus62;
+}
+
+var roadTimeMonday = new roadTime (5,11,6,9);
 
 roadTime.setTime = function (newTimeBus52, newTimeTram) {
   roadTime.bus52 = newTimeBus52;
