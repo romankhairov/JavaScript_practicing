d3c729e3125cdbba2236b05dc693965ce6b359d3
Roman Khairov <roman.khairov@gmail.com>
2017-02-04 22:23:06 +0100

    Add method to constructor

--- 0014_TimeToWorkUsingMethods.js
+++ 0014_TimeToWorkUsingMethods.js
@@ -3,20 +3,18 @@ function roadTime(walk, bus52, tram, bus62) {
   this.bus52 = bus52;
   this.tram = tram;
   this.bus62 = bus62;
+  this.calcTotalTime = function() {
+      return this.walk + this.bus52 + this.tram + this.bus62;
+  };
 }
 
 var roadTimeMonday = new roadTime (5,11,6,9);
+var roadTimeTuesday = new roadTime (6,10,7,11);
+var roadTimeWednesday = new roadTime (8,11,9,5);
+var roadTimeThursday = new roadTime (5,10,7,7);
+var roadTimeFriday = new roadTime (5,12,5,12);
 
-roadTime.setTime = function (newTimeBus52, newTimeTram) {
-  roadTime.bus52 = newTimeBus52;
-  roadTime.tram = newTimeTram;
-};
+var totalTime = roadTimeMonday.calcTotalTime() + roadTimeTuesday.calcTotalTime() + roadTimeWednesday.calcTotalTime() + roadTimeThursday.calcTotalTime() + roadTimeFriday.calcTotalTime();
+var averageTime = totalTime/5;
 
-roadTime.setTime(10,15);
-
-roadTime.getTotalTime = function () {
-  return (roadTime.walk + roadTime.bus52 + roadTime.tram + roadTime.bus62);
-};
-
-
-console.log(roadTime.getTotalTime());
+console.log(averageTime);
