29632253cabe57e3a6e2b3377927a1846a37265a
Roman Khairov <roman.khairov@gmail.com>
2017-02-19 21:48:48 +0100

    Add  objects with footballers

---
+++ 0015_BestFootballPlayers.js
@@ -0,0 +1,44 @@
+var cristianoRonaldo = {
+    name: "Cristiano Ronaldo",
+    position: "LW",
+    preferedFoot: "Right",
+    weakFoot: 4,
+    skillMoves: 5,
+    overall: 94
+    pace: 92,
+    shooting: 92,
+    passing: 81,
+    dribbling: 91,
+    defending: 33,
+    physical: 80,
+}
+
+var lionelMessi = {
+    name: "Lionel Messi",
+    position: "RW",
+    preferedFoot: "Left",
+    weakFoot: 4,
+    skillMoves: 4,
+    overall: 93,
+    pace: 89,
+    shooting: 90,
+    passing: 86,
+    dribbling: 96,
+    defending: 26,
+    physical: 61,
+}
+
+var neymar = {
+    name: "Neymar da Silva Santos Jr.",
+    position: "LW",
+    preferedFoot: "Right",
+    weakFoot: 5,
+    skillMoves: 5,
+    overall: 92,
+    pace: 91,
+    shooting: 84,
+    passing: 78,
+    dribbling: 95,
+    defending: 30,
+    physical: 56,
+}
