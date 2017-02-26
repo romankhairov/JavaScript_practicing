e660ad2fe87144a6cc4b448d9e60bb6ff31f3d0f
Roman Khairov <roman.khairov@gmail.com>
2017-02-26 22:51:07 +0100

    Add possibility to change the number of sold units

--- 0016_BicycleShopSystem.js
+++ 0016_BicycleShopSystem.js
@@ -2,8 +2,8 @@ var accountBalance = {
   total:0,
   add: function(price) {
       this.total += price;
-
-  }
+      this.lastSellAmount = price;
+  },
   sell: function(unit, amount)
     switch (unit) {
       case "a": this.add(0,00 * amount);
@@ -14,6 +14,20 @@ var accountBalance = {
         break;
     }
     return true;
-},
+  },
+  cancelSell: function() {
+      this.total -= this.lastSellAmount;
+      this.lastSellAmount = 0;
+    }
+
+};
+
+accountBalance.sell("a",2);
+accountBalance.sell("b",1);
+accountBalance.sell("c",1);
+
+
+accountBalance.cancelSell();
+accountBalance.sell("a",1)
 
 console.log("Your actual account balance is " + accountBalance.total);
